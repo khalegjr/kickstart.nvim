e79d555fc13366f1f88b5525fdfcf49598fa46c2
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2025-01-15 02:40:53 -0300

    Fix o carregamento de módulos

--- init.lua
+++ init.lua
@@ -93,9 +93,6 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
